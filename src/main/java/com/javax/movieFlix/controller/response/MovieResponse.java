75a8f0587707ce7f254c022e23188d1ddeea52fa
Damori404 <daviestudos2k20@gmail.com>
2025-09-12 19:53:54 -0300

    feat(Movie): Create Movie DTO dor request and response

---
+++ src/main/java/com/javax/movieFlix/controller/response/MovieResponse.java
@@ -0,0 +1,20 @@
+package com.javax.movieFlix.controller.response;
+
+import com.javax.movieFlix.entity.Category;
+import lombok.Builder;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Builder
+public record MovieResponse (
+        Long id,
+        String title,
+        String description,
+        LocalDate releaseDate,
+        double rating,
+        List<CategoryResponse> categories,
+        List<StreamingResponse> streamings 
+) {
+
+}
