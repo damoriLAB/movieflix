2effa0fad2e0c6fc9da94b2e435f718476efb32e
Damori404 <daviestudos2k20@gmail.com>
2025-09-12 19:54:40 -0300

    feat(Movie): Create Movie controller

---
+++ src/main/java/com/javax/movieFlix/controller/MovieController.java
@@ -0,0 +1,54 @@
+package com.javax.movieFlix.controller;
+
+import com.javax.movieFlix.controller.request.MovieRequest;
+import com.javax.movieFlix.controller.response.MovieResponse;
+import com.javax.movieFlix.entity.Movie;
+import com.javax.movieFlix.mapper.MovieMapper;
+import com.javax.movieFlix.service.MovieService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/movieflix/movie")
+@RequiredArgsConstructor
+public class MovieController {
+    private final MovieService movieService;
+
+    @GetMapping
+    public ResponseEntity<List<MovieResponse>> findAll(){
+        return ResponseEntity.ok(
+          movieService.findAll().stream()
+                  .map(MovieMapper::toMovieResponse)
+                  .toList()
+        );
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MovieResponse> findByID(@PathVariable Long id){
+        Optional<Movie> optionalMovie = movieService.findByID(id);
+        return ResponseEntity.ok(optionalMovie.map(MovieMapper::toMovieResponse)
+                .orElse(null)
+        );
+    }
+
+    @PostMapping
+    public ResponseEntity<MovieResponse> save(@RequestBody MovieRequest movieRequest){
+        Movie savedMovie = movieService.save(MovieMapper.toMovie(movieRequest));
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(MovieMapper.toMovieResponse(savedMovie));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteByID(@PathVariable Long id){
+        movieService.deleteByID(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+
+}
