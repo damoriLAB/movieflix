ee5f66fccb6dcb921381fef34d78b9d7fad48104
Damori404 <daviestudos2k20@gmail.com>
2025-09-16 22:33:41 -0300

    feat(repository): add findMovieByCategories

--- src/main/java/com/javax/movieFlix/repository/MovieRepository.java
+++ src/main/java/com/javax/movieFlix/repository/MovieRepository.java
@@ -1,7 +1,12 @@
 package com.javax.movieFlix.repository;
 
+import com.javax.movieFlix.entity.Category;
 import com.javax.movieFlix.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+    List<Movie> findMovieByCategories(List<Category> categories);
 }
